283a7dd4c974f818e7f967b2278812f9282263c4
huangzhimao1 <huangzhimao@jd.com>
2018-10-30 14:29:00 +0800

    加入条件queryWrapper

--- src/main/java/com/fx/App.java
+++ src/main/java/com/fx/App.java
@@ -1,5 +1,8 @@
 package com.fx;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
@@ -9,6 +12,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -28,5 +32,15 @@ public class App
         columnMap.put("district","Kabol2");
         List<City> cityList=cityMapper.selectByMap(columnMap);
         System.out.println(cityList);
+        List<Integer> idList=new ArrayList<Integer>();
+        //idList.add(1);
+        //idList.add(2);
+        /*for (int i = 1; i <=10001 ; i++) {
+            idList.add(i);
+        }
+        int count=cityMapper.deleteBatchIds(idList);
+        System.out.println("删除记录数:"+count);*/
+        List<City> cityList2=cityMapper.selectList(new QueryWrapper<City>().gt("id",2000));
+        System.out.println(cityList2);
     }
 }
